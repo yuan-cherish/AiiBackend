48df14bfb5a22cce033539a0fc4cc3291ce21284
yuan-cherish <940575974@qq.com>
2023-06-16 11:16:06 +0800

    https_0616

--- src/main/java/com/example/interviewbackend/InterviewBackendApplication.java
+++ src/main/java/com/example/interviewbackend/InterviewBackendApplication.java
@@ -1,8 +1,15 @@
 package com.example.interviewbackend;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @MapperScan("com.example.*.mapper")
@@ -10,5 +17,32 @@ public class InterviewBackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(InterviewBackendApplication.class, args);
+    }
+
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory(){
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+        tomcat.addAdditionalTomcatConnectors(httpConnector());
+        return tomcat;
+    }
+
+    @Bean
+    public Connector httpConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        connector.setPort(8081); // 监听Http的端口
+        connector.setSecure(false);
+        connector.setRedirectPort(8443); // 监听Http端口后转向Https端口
+        return connector;
     }
 }
